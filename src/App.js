89b04a52fb0be37d22741035e153ca6234551db3
gawaxx <jules.blanc@unccub.com>
2020-01-08 14:06:46 +0000

    Getting a navBar working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
+import { BrowserRouter as Router } from 'react-router-dom' //Route, Link, Switch
+import { Container } from 'semantic-ui-react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <Router>
+        <Container className="container">
+
+          
+          <Container className="navBar">
+            <NavBar />
+          </Container>
+          {/* <Route exact path='/login' component={} />
+
+          <Container className="links">
+              <Route exact path='/login' component={LoginForm} />
+              <Route exact path='/allCards' component={AllCards} />
+          </Container> */}
+          
+        </Container>
+			</Router>
   );
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,47 +1,20 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Container, Button, Form, Grid, Header, Message, Segment, Icon} from 'semantic-ui-react';
-
+import './NavBar.css';
 
 export class NavBar extends Component {
     render() {
         return (
-            <Grid centered columns={5}>
-                <Grid.Row fluid>
-					<Segment>
-						<NavLink className='bottom-nav-link' to="/allCards" exact>
-							<Icon name='home' size='large' />
-						</NavLink>
-
-					</Segment>
-
-					<Segment>
-                        <NavLink
-                        className='bottom-nav-link' to="/map" exact>
-                            <Icon name='map marker alternate' size='large' />
-                        </NavLink>
-					</Segment>
-
-					<Segment>
-                        <NavLink className='bottom-nav-link' to="/" exact>
-                            <Icon name='calendar plus outline' size='large' />
-                        </NavLink>
-					</Segment>
-
-					<Segment>
-                        <NavLink className='bottom-nav-link' to="/" exact>
-                            <Icon name='star' size='large' />
-                        </NavLink>
-					</Segment>
-
-					<Segment>
-                        <NavLink className='bottom-nav-link' to="/" exact>
-                            <Icon name='user circle' size='large' />
-                        </NavLink>
-					</Segment>
-                    
-			</Grid.Row>
-		</Grid>
+            <div className="NavBar">
+                <div className="company-Logo">
+                    <h1>Company Logo</h1>
+                </div>
+                <div className="search-bar">
+                    <p> search bar </p>
+                </div>
+                <div className="account-info">
+                    <p> user info stuff</p>
+                </div>
+            </div>
         );
     }
 }
