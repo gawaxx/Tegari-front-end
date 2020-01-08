9e49b005d01056baa2c46bc198c19135fb85b646
gawaxx <jules.blanc@unccub.com>
2020-01-08 14:41:58 +0000

    Some styling

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './NavBar.css';
 
 const SearchBar = props => {
     return (
-        <div>
+        <div className="Form">
             <form onSubmit={e => props.handleSubmit(e)} >
                 <label>
                     Q:
                     <input type="text" name="name" />
                 </label>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" className="SubmitButton"/>
             </form>
         </div>
     );
