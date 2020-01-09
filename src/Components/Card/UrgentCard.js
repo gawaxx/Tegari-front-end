d928c0fa32afc6681e69d0b14a0209501b90872d
gawaxx <jules.blanc@unccub.com>
2020-01-09 15:22:28 +0000

    Adding card for urgent stuff

---
+++ src/Components/Card/UrgentCard.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+
+
+export class UrgentCard extends Component {
+    render() {
+        const {category, user_id, id, title, price, postcode, city, description, image_url, condition, created_at} = this.props.post
+        return (
+            <Card>
+            <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+                <Image src={image_url} wrapped ui={false} />
+                    <Card.Content>
+                    <Card.Header>Matthew</Card.Header>
+                    <Card.Meta>
+                        <span className='date'>Joined in 2015</span>
+                    </Card.Meta>
+                    <Card.Description>
+                        Matthew is a musician living in Nashville.
+                    </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                    <a>
+                        <Icon name='user' />
+                        22 Friends
+                    </a>
+                </Card.Content>
+            </Card>
+        );
+    }
+}
+
+export default UrgentCard;
