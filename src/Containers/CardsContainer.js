2775306dd91f87ad2fdf69a086861a2883c55a0e
gawaxx <jules.blanc@unccub.com>
2020-01-09 11:23:36 +0000

    Preparing cards

---
+++ src/Containers/CardsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { APILINK, GetAPI, PostAPI, PatchAPI, DeleteAPI, API } from '../Components/API.js'
+
+
+export class CardsContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CardsContainer;
