70c8ea90c9c603c172d3e4fb9a505735997ac942
gawaxx <jules.blanc@unccub.com>
2020-01-09 15:38:15 +0000

    Urgent cards display to the left now

--- src/Components/Card/UrgentCard.js
+++ src/Components/Card/UrgentCard.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react'
-
-
+import '../Component.css'
 
 export class UrgentCard extends Component {
     render() {
         const {category, user_id, id, title, price, postcode, city, description, image_url, condition, created_at} = this.props.post
         return (
-            <Card>
-                <Image src={image_url} wrapped ui={false} />
-                    <Card.Content>
-                    <Card.Header>Matthew</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>Joined in 2015</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        Matthew is a musician living in Nashville.
-                    </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                    <a>
-                        <Icon name='user' />
-                        22 Friends
-                    </a>
-                </Card.Content>
-            </Card>
+            <div>
+                <div class="Urgentcard">
+                    <img src={image_url} alt="urgent card" />
+                    <div class="Urgentcontainer">
+                        <h4><b>{title}</b></h4>
+                        <p>{description}</p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
