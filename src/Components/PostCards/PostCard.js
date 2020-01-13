c325a22280d15c7925e918d038661db9911a162a
gawaxx <jules.blanc@unccub.com>
2020-01-13 10:20:17 +0000

    can view and go back a post

--- src/Components/PostCards/PostCard.js
+++ src/Components/PostCards/PostCard.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import './PostCard.css'
 
 export class PostCard extends Component {
+
+    handleClick = () => {
+        this.props.deRenderAPost()
+    }
     render() {
         const {category, user_id, id, title, price, postcode, city, description, image_url, condition, created_at} = this.props.post
 
         return (
-            <div>
-                <h1>title</h1>
-                <h2>postcode, city</h2>
-                <p>category</p>
+            <div className="wrapper">
+
+                <div className="tips"></div>
+
+                <div className="PostInfo">
+                    <p onClick={ () => {this.handleClick()} }>Go Back</p>
+                    <img src={image_url}></img>
+                    <h1>{title}</h1>
+                    <h2>{postcode}, {city}</h2>
+                    <p>{category}</p>
+                    <p>{condition}</p>
+                    <p>{description}</p>
+                </div>
+
+                <div className="sellerInfo"></div>
+                
             </div>
         );
     }
