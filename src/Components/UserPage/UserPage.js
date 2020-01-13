202160f9ac3c7d53efd1dbc64aacb9ec3986ecf4
gawaxx <jules.blanc@unccub.com>
2020-01-13 14:27:52 +0000

    Displaying the user info

--- src/Components/UserPage/UserPage.js
+++ src/Components/UserPage/UserPage.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
 export class UserPage extends Component {
-    
+
     handleClick = () => {
         debugger
     }
 
     render() {
+        const { user_name, name, points } = this.props.user
         return (
-            <div onClick={() => this.handleClick()}>
-                hello {this.props.user.name}
+            <div>
+                <h1>You are currently viewing the profile of: {user_name} </h1> 
+                <h1> Their name: {name} </h1> 
+                <h1> {points} points </h1>
             </div>
         );
     }
