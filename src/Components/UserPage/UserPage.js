f23399d81b40af694fe9f9281806a367a9bfa3e2
gawaxx <jules.blanc@unccub.com>
2020-01-13 14:49:11 +0000

    Some styling

--- src/Components/UserPage/UserPage.js
+++ src/Components/UserPage/UserPage.js
@@ -11,17 +11,20 @@ export class UserPage extends Component {
         const { user_name, name, points } = this.props.user
         return (
             <div className="wrapper" >
-                <div>
+                <div className="left">
 
                 </div>
 
-                <div>
-                    <h1>You are currently viewing the profile of: {user_name} </h1> 
+                <div className="center">
+                    <div className="username" >
+                        <h1> {user_name} </h1> 
+                    </div>
+
                     <h1> Their name: {name} </h1> 
                     <h1> {points} points </h1>
                 </div>
-                
-                <div>
+
+                <div className="right">
 
                 </div>
             </div>
