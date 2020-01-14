ca710f9c7d495cacc65c0e0a9661cd50306161ec
gawaxx <jules.blanc@unccub.com>
2020-01-14 13:57:44 +0000

    Adding drop down for condition

--- src/Components/PostCreation/PostCreation.js
+++ src/Components/PostCreation/PostCreation.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import '../AuthForms.css'
 
 export class PostCreation extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+      }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+
     render() {
         return (
             <div>
@@ -17,6 +28,36 @@ export class PostCreation extends Component {
                             <input type="description" name="description" placeholder="Description" />
                             <span> Description </span>
                         </label>
+
+                        <label>
+                            <input type="postalcode" name="postalcode" placeholder="Postal Code" />
+                            <span> Postal Code </span>
+                        </label>
+
+                        <label>
+                            <input type="email" name="email" placeholder="E-mail" />
+                            <span> E-mail </span>
+                        </label>
+
+                        <label>
+                            <input type="city" name="city" placeholder="City" />
+                            <span> City </span>
+                        </label>
+
+                        <label>
+                            <input type="phonenumber" name="phonenumber" placeholder="Phone Number" />
+                            <span> Phone Number </span>
+                        </label>
+
+                        <label>
+                            <select onChange={this.handleChange}>
+                                <option value="New">New</option>
+                                <option value="Almostnew">Almost new</option>
+                                <option value="Used">Used</option>
+                                <option value="Veryused">Very used</option>
+                            </select>
+                        </label>
+
 
                         <input type="submit" />
                     </form> 
