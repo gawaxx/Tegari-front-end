f155d567479ebb3ccf5fcabaa6316967d8afc9d4
gawaxx <jules.blanc@unccub.com>
2020-01-14 13:44:39 +0000

    Navbar has box shadow

--- src/Components/PostCreation/PostCreation.js
+++ src/Components/PostCreation/PostCreation.js
@@ -4,20 +4,23 @@ import '../AuthForms.css'
 export class PostCreation extends Component {
     render() {
         return (
-            <div className="FormContainer">
-                <form>
-                    <label>
-                        <input type="title" name="title" placeholder="Title" />
-                        <span> Title </span>
-                    </label>
+            <div>
+                <img src="https://via.placeholder.com/250" alt="placeholder" />
+                <div className="FormContainer">
+                    <form>
+                        <label>
+                            <input type="title" name="title" placeholder="Title" />
+                            <span> Title </span>
+                        </label>
 
-                    <label>
-                        <input type="description" name="description" placeholder="Description" />
-                        <span> Description </span>
-                    </label>
+                        <label>
+                            <input type="description" name="description" placeholder="Description" />
+                            <span> Description </span>
+                        </label>
 
-                    <input type="submit" />
-                </form> 
+                        <input type="submit" />
+                    </form> 
+                </div>
             </div>
         );
     }
