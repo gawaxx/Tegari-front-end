d1dd84641d9d33ba29077fd8ded55c68aeb59964
gawaxx <jules.blanc@unccub.com>
2020-01-14 13:28:07 +0000

    Starting form creation

--- src/Components/PostCreation/PostCreation.js
+++ src/Components/PostCreation/PostCreation.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import '../AuthForms.css'
 
 export class PostCreation extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="FormContainer">
+                <form>
+                    <h2>Create a Post</h2>
+                    <label>
+                        <input type="email" name="email" placeholder="E-mail" />
+                        <span> E-mail </span>
+                    </label>
+
+                    <label>
+                        <input type="password" name="password" placeholder="Password" />
+                        <span> Password </span>
+                    </label>
+
+                    <input type="submit" />
+                </form> 
             </div>
         );
     }
