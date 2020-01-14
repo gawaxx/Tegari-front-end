834cbb7059fac8bab3a6208dc15ea8a311649fce
gawaxx <jules.blanc@unccub.com>
2020-01-14 13:23:45 +0000

    Can get to /create from front page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { APILINK, API } from './Components/API.js'
 import LandingPage from "./Containers/LandingPage";
 import UserContainer from "./Containers/UserContainer";
 import PostCard from "./Components/PostCards/PostCard";
+import PostCreation from "./Components/PostCreation/PostCreation";
 
 
 // {/* <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" /> */}
@@ -48,7 +49,7 @@ function App() {
           <Container className="links">
               <Route exact path="/users/:id" component={UserContainer}/>
               <Route exact path="/posts/:id" component={PostCard}/>
-              <Route exact path="" component={} />
+              <Route exact path="/create" component={PostCreation} />
               {/* <Route exact path='/users' component={UserContainer} /> */}
               <Route exact path='/' component={LandingPage} />
               <Route exact path='/search' render={() => <CardsContainer search={search} /> }/>
