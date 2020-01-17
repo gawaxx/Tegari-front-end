935d2d7de6e8e706fa869f509a6796063f1d0b7c
gawaxx <jules.blanc@unccub.com>
2020-01-17 13:47:12 +0000

    Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import NavBar from './Components/NavBar';
 import { Route, Redirect, useHistory } from 'react-router-dom' // Link, Switch
-import { Container } from 'semantic-ui-react';
 import CardsContainer from './Containers/CardsContainer';
 import AuthFormsContainer from "./Containers/AuthFormsContainer";
 import { APILINK, API } from './Components/API.js'
@@ -11,6 +10,7 @@ import UserContainer from "./Containers/UserContainer";
 import PostCard from "./Components/PostCards/PostCard";
 import PostCreation from "./Components/PostCreation/PostCreation";
 import MyAccount from "./Components/AccountCard/MyAccount";
+import { Container, Grid, Header, List, Segment } from 'semantic-ui-react';
 
 
 
@@ -62,31 +62,66 @@ function App(props) {
 
   return (
     <>
-    <Container className="navbar_container">
-      <Route path='/' render={() => <NavBar user={user} handleSearchSubmit={handleSearchSubmit} logout={logout}/> } />
-    </Container>
-        <Container>
-         
-          {/* <Route exact path='/login' component={} /> */}
 
-          <Container className="sublinks">
-              <Route exact path="/users/:id" component={UserContainer}/>
-              <Route exact path="/posts/:id" render={() => <PostCard user={user} /> }/>
-              <Route exact path="/create" render={() => <PostCreation user={user} />}  />
-              {/* <Route exact path="/my_profile" render={() => < MyAccount getUserData={getUserData} user={user} />} /> */}
-              <Route exact path="/my_profile" >
-                {loggedIn ? < MyAccount getUserData={getUserData} user={user} logOut={logOut} /> : <Redirect to='/login' />}
-              </Route>
-              <Route exact path='/' component={LandingPage} />
-              <Route exact path='/search' render={() => <CardsContainer search={search} /> }/>
-              {/* <Route exact path='/login' render={() => <AuthFormsContainer user={user} handleLogin={handleLogin} handleSignup={handleSignup} setUser={setUser} /> }  /> */}
-              <Route exact path="/login" >
-                {loggedIn ? <Redirect to='/my_profile' /> : <AuthFormsContainer user={user} handleSignup={handleSignup} setUser={setUser} />}
-              </Route>
-          </Container>
-          
+      <Container className="navbar_container">
+        <Route path='/' render={() => <NavBar user={user} handleSearchSubmit={handleSearchSubmit} logout={logout} />} />
+      </Container>
+
+      <Container>
+
+        <Container className="sublinks">
+          <Route exact path="/users/:id" component={UserContainer} />
+          <Route exact path="/posts/:id" render={() => <PostCard user={user} />} />
+          <Route exact path="/create" render={() => <PostCreation user={user} />} />
+          {/* <Route exact path="/my_profile" render={() => < MyAccount getUserData={getUserData} user={user} />} /> */}
+          <Route exact path="/my_profile" >
+            {loggedIn ? < MyAccount getUserData={getUserData} user={user} logOut={logOut} /> : <Redirect to='/login' />}
+          </Route>
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/search' render={() => <CardsContainer search={search} />} />
+          {/* <Route exact path='/login' render={() => <AuthFormsContainer user={user} handleLogin={handleLogin} handleSignup={handleSignup} setUser={setUser} /> }  /> */}
+          <Route exact path="/login" >
+            {loggedIn ? <Redirect to='/my_profile' /> : <AuthFormsContainer user={user} handleSignup={handleSignup} setUser={setUser} />}
+          </Route>
         </Container>
-			</>
+
+      </Container>
+
+      <Segment className="footer" inverted vertical style={{ padding: '2em 0em' }}>
+        <Container>
+          <Grid divided inverted stackable>
+            <Grid.Row>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='About' />
+                <List link inverted>
+                  <List.Item as='a'>Sitemap</List.Item>
+                  <List.Item as='a'>Contact Us</List.Item>
+                  <List.Item as='a'>Religious Ceremonies</List.Item>
+                  <List.Item as='a'>Gazebo Plans</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Services' />
+                <List link inverted>
+                  <List.Item as='a'>Banana Pre-Order</List.Item>
+                  <List.Item as='a'>DNA FAQ</List.Item>
+                  <List.Item as='a'>How To Access</List.Item>
+                  <List.Item as='a'>Favorite X-Men</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Header as='h4' inverted>
+                  Footer Header
+              </Header>
+                <p>
+                  *App name* made with <span role="img" > 🥖 </span> by Jules Blanc, 2020.
+              </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
+    </>
   );
 }
 
