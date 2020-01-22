ae35d021cd5eb160d297d56b748ab190339c4e5e
gawaxx <jules.blanc@unccub.com>
2020-01-22 11:16:03 +0000

    User page css

--- src/Components/UserPage/AllPostUser.js
+++ src/Components/UserPage/AllPostUser.js
@@ -4,16 +4,28 @@ export class AllPostUser extends Component {
     render() {
         const { title, price, postcode, city, image_url, created_at} = this.props.post
         return (
-            <div className="Postcard">
-                <div>
-                    <img src={image_url} alt="the" />
+
+            <div className="postCardWrapper">
+                <div className="postcardright">
+
                 </div>
-                <div className="PostcardContainer">
-                    <h4><b>{title}</b></h4>
-                    <p> £{price} <br></br> {postcode}, {city} </p>
-                    <p> posted on {created_at.split("T")[0]} </p>
+
+                <div className="Postcard">
+                    <div>
+                        <img src={image_url} alt="the" />
+                    </div>
+                    <div className="PostcardContainer">
+                        <h4><b>{title}</b></h4>
+                        <p> £{price} <br></br> {postcode}, {city} </p>
+                        <p> posted on {created_at.split("T")[0]} </p>
+                    </div>
+                </div>
+
+                <div className="postcardleft">
+
                 </div>
             </div>
+
         );
     }
 }
